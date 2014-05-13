8b3b22f672a6945c292102e43b8ecb7cc689580b
Erin Boyle <eekboyle@gmail.com>
2014-05-13 08:03:00 -0700

    Customer Lists
    added request to fetch and display customer lists

--- app.js
+++ app.js
@@ -2,7 +2,27 @@
 
   return {
     events: {
-      'app.activated':'init'
+      'app.activated':'init',
+      'pane.activated': 'getCustomerLists'
+    },
+
+    requests: {
+      customerLists: function(){
+        return{
+          url: '/api/v2/user_views.json',
+          type: 'GET',
+          dataType: 'json'
+        };
+      }
+    },
+    getCustomerLists: function(){
+      var request = this.ajax('customerLists');
+      request.done(this.displayForm);
+      //request.fail(this.showError);
+    },
+
+    displayForm: function(data){
+      this.switchTo('main', data);
     },
 
     init: function() {
