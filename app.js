0cb6211902effc66fc14a37707457426c8b2380b
Jesper Christiansen <jchristiansen@zendesk.com>
2014-05-13 08:03:04 -0700

    Form functionality

--- app.js
+++ app.js
@@ -2,11 +2,24 @@
 
   return {
     events: {
-      'app.activated':'init'
+      'app.activated':'init',
+      'click .save':'saveClicked'
     },
 
     init: function() {
       this.switchTo('setup');
+    },
+
+    saveClicked: function() {
+      console.log(this.getField('campaign-name'));
+      console.log(this.getField('subkect'));
+      console.log(this.getField('tags'));
+      console.log(this.getField('description'));
+    },
+
+    getField: function(name) {
+      var cssSelector = '.' + name;
+      return this.$(cssSelector).val();
     }
   };
 
